b88708b3eaeacbc9ab11c6a2271423b782e0ed80
Paul Jensen <paulbjensen@gmail.com>
2013-10-15 23:39:22 +0100

    Added some changes to support using Redstone 0.0.5

--- tasks/redstone.js
+++ tasks/redstone.js
@@ -10,33 +10,60 @@
 
 var Redstone = require('redstone');
 
+
+// This function injects variables into commands
+//
+function insertVariablesIntoCommands (commands, variables) {
+  var command, commandName, variable, variableName;
+  for (commandName in commands) {
+    command = commands[commandName];
+    for (variableName in variables) {
+      variable = variables[variableName];
+      commands[commandName] = commands[commandName].replace(variableName, variable);
+    }
+  }
+  return commands;
+}
+
+
+
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('redstone', 'deploying Node.js applications using redstone', function(runCommands) {
+  grunt.registerMultiTask('redstone', 'deploying Node.js applications using redstone', function () {
 
-    var done = this.async();
 
-    var options = this.options({
-      ssh: {
-        hostname   : null,
-        user       : null
-      },
-      commands: {
-        list_files: "ls -ll",
-        print_date: "date"
-      },
-      runCommands: []
-    });
 
-    var redstone = new Redstone(options);
+    var done      = this.async();
 
-    redstone.runCommands(runCommands || options.runCommands || [], function(err) {
+
+
+    // Insert variables into commands, if they are passed in the options
+    //
+    if (this.data.variables) {
+      this.data.commands = insertVariablesIntoCommands(this.data.commands, this.data.variables);
+    }
+
+
+    // Initialize Redstone
+    //
+    var redstone  = new Redstone(this.data);
+
+
+
+    // Execute Redstone
+    //
+    var self = this;
+    redstone.runCommands(this.data.commandsInOrder, function(err) {
+      grunt.log.writeln(self.target + ': ' + self.data);
+
       if(err) { return done(err); }
-      grunt.log.writeln('Commands: "' + options.runCommands.join(', ') + '" run successfully.');
+      grunt.log.writeln('Commands: "' + self.options.commandsInOrder.join(', ') + '" run successfully.');
     });
+
+
 
   });
 
